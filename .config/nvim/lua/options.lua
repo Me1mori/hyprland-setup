cb2333ae5ecee40f2db29ef42d08bc11aa0ffe0a
Me1mori <ssalvarezleonardoaa@gmail.com>
2026-07-03 14:33:20 -0700

    Agregando Yazi config

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -10,3 +10,22 @@ vim.g.vimsence_client_id = "439485121000210432"
 vim.g.vimsence_editing_details = "Editando: {}"
 vim.g.vimsence_editing_state = "En el archivo: {}"
 
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 1 then
+      local arg = vim.fn.argv(0)
+      -- Validar explícitamente que es "string"
+      if type(arg) == "string" then
+        if vim.fn.isdirectory(arg) == 1 then
+          vim.cmd("cd " .. arg)
+        elseif vim.fn.filereadable(arg) == 1 then
+          local dir = vim.fn.fnamemodify(arg, ":p:h")
+          if vim.fn.getcwd() == vim.fn.expand("~") then
+            vim.cmd("cd " .. dir)
+          end
+        end
+      end
+    end
+  end,
+})
+
